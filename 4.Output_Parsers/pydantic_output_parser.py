b849f8ac42f5aea7fb47a92775894ba5ec1b7e44
Shreyas Walde <waldeshreyas@gmail.com>
2025-08-15 17:31:40 +0530

    parsers

---
+++ 4.Output_Parsers/pydantic_output_parser.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema 
+
+load_dotenv()
+
+# Define the model (if using HF API)
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
