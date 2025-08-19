1cca5f2fc40125b7866950659c28c92963cbd508
Shreyas Walde <waldeshreyas@gmail.com>
2025-08-19 21:41:06 +0530

    Runnable

---
+++ 7.Runnables_indetail/Runnable_sequence.py
@@ -0,0 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableSequence
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+prompt1 = PromptTemplate(
+    template='write a joke about{topic}',
+    input_variables=['topic']
+)
+
+model = ChatGoogleGenerativeAI(model='gemini-1.5-flash')
+
+parser = StrOutputParser()
+
+chain = RunnableSequence(prompt1, model, parser)
+
+print(chain.invoke({'topic':'AI'}))
