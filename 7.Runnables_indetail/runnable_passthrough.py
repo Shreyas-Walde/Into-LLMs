9d27e5d27efc686413a491c905d0b6d1316e1ce9
Shreyas Walde <waldeshreyas@gmail.com>
2025-08-19 23:39:17 +0530

    runnable passthrough

--- 7.Runnables_indetail/runnable_passthrough.py
+++ 7.Runnables_indetail/runnable_passthrough.py
@@ -1,10 +1,11 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain.schema.runnable import RunnableSequence
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 prompt1 = PromptTemplate(
     template='write a joke about{topic}',
@@ -20,6 +21,12 @@ prompt2 = PromptTemplate(
     input_variables=['text']
 )
 
-chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+joke_chain = RunnableSequence(prompt1, model, parser)
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explaination': RunnableSequence(prompt2, model, parser)
+})
 
-print(chain.invoke({'topic':'AI'}))
+chain = RunnableSequence(joke_chain, parallel_chain)
+
+print(chain.invoke({'topic':'Hans Zimmers music'}))
